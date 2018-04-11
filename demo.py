10d9a9414ae96d4cfb50422949d6979075319436
ipowe <ipowerink@outlook.com>
2018-04-11 21:59:35 +0800

    enumerate:索引-元素  对形式，测试

--- demo.py
+++ demo.py
@@ -1,10 +1,19 @@
 
-
-s = "   abc   "
-while s[:1] == ' ':
-    s=s[1:]
-    print(-1)
+s=['a','b']
+for i, v in enumerate(s):
+    s[i] = 'c'
 print(s)
+
+
+#
+# s = "   abc   "
+# while s[:1] == ' ':
+#     s=s[1:]
+#     print(-1)
+# print(s)
+
+
+
 # def f1(a,*,b):
 #     print(b)
 # f1(1,**{'b':1})
